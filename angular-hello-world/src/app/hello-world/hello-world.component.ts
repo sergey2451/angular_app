61c4fc67dc9e9823160d7bcc4ca04734d2309422
Sergey Kragel <kragel.s@mail.ru>
2022-09-02 18:05:23 +0300

    1

--- angular-hello-world/src/app/hello-world/hello-world.component.ts
+++ angular-hello-world/src/app/hello-world/hello-world.component.ts
@@ -2,11 +2,6 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-hello-world',
-  template: `
-    <p>
-      hello-world works inline!
-    </p>
-  `,
   templateUrl: './hello-world.component.html',
   styleUrls: ['./hello-world.component.css']
 })
