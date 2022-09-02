af4ccc23c1b484b51bc672f82e306e40eb3bf883
Sergey Kragel <kragel.s@mail.ru>
2022-09-02 16:44:41 +0300

    first commit

---
+++ angular-hello-world/src/app/hello-world/hello-world.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hello-world',
+  template: `
+    <p>
+      hello-world works inline!
+    </p>
+  `,
+  templateUrl: './hello-world.component.html',
+  styleUrls: ['./hello-world.component.css']
+})
+export class HelloWorldComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
