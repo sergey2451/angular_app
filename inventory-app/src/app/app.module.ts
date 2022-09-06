0a27e013a0d8213618f6fb071cbf7d7e484e85c8
Sergey Kragel <kragel.s@mail.ru>
2022-09-06 18:01:43 +0300

    2

---
+++ inventory-app/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductsListComponent } from './products-list/products-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductsListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
